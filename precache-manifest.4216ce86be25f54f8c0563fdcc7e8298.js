4c2f953ecc2efc5b30ce162b28f0fd50f1665cba
CK Pantelides <ckpantelides@gmail.com>
2020-10-13 18:05:01 +0100

    Updates

---
+++ precache-manifest.4216ce86be25f54f8c0563fdcc7e8298.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9121c68ed637b40ea92fd277f2a2a1f",
+    "url": "/duckduck-admin/index.html"
+  },
+  {
+    "revision": "f2d2b36111f699597029",
+    "url": "/duckduck-admin/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f933e8ef90846b5019ba",
+    "url": "/duckduck-admin/static/js/2.75737173.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/duckduck-admin/static/js/2.75737173.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2d2b36111f699597029",
+    "url": "/duckduck-admin/static/js/main.11e01f36.chunk.js"
+  },
+  {
+    "revision": "ca4cb14d0186b58c8f38",
+    "url": "/duckduck-admin/static/js/runtime-main.ade1a86c.js"
+  }
+]);
